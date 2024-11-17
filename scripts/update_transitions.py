5f71c720e53c2ba760f4c2390f385397d5f626e9
Alexander Wagner <alexander.wagner@aisec.fraunhofer.de>
2024-11-17 11:32:27 +0100

    scripts: Add transition handling

---
+++ scripts/update_transitions.py
@@ -0,0 +1,94 @@
+from git import Repo
+from github import Auth
+from github import Github
+from github import GithubIntegration
+from github import NamedUser
+import sys
+
+import bibtex_handler as bh
+from TOKEN import TOKEN
+
+
+class Transition:
+    def __init__(self, issue, command):
+        self.issue = issue
+        assert command in ["yes", "no"]
+        self.command = command
+
+
+def main():
+    auth = Auth.Token(TOKEN)
+    g = Github(auth=auth)
+    gh_repo = g.get_repo("aewag/physical-attack-collection")
+
+    issues = gh_repo.get_issues(labels=["in-review"])
+    transitions = list()
+
+    repo = Repo(".")
+    if repo.is_dirty():
+        print("Repository is dirty. Cannot continue.")
+        exit()
+    repo.heads.develop.checkout()
+
+    for issue in issues:
+        print(issue.title)
+        for comment in issue.get_comments():
+            if comment.user.login != "aewag":
+                print(f"Unknown user {comment.user} commented in {issue.title}")
+                continue
+            command = comment._rawData["body"]
+            if command not in ["yes", "no"]:
+                print(f"Unhandled comment {command} in {issue.title}")
+                continue
+            break
+        else:
+            print(f"No updates received for {issue.title}")
+            continue
+        transitions.append(Transition(issue, command))
+
+    for transition in transitions:
+        # Read bibtex files
+        in_review = bh.read_bibtex(bh.IN_REVIEW_FP)
+        in_scope = bh.read_bibtex(bh.IN_SCOPE_FP)
+        not_in_scope = bh.read_bibtex(bh.NOT_IN_SCOPE_FP)
+        # Retrieve bibtex entry of publication from in_review
+        publication = [
+            e for e in in_review.entries if e["ID"] == transition.issue.title
+        ]
+        assert len(publication) == 1
+        publication = publication[0]
+        # Remove transition from in_review
+        in_review.entries = [e for e in in_review.entries if e != publication]
+        # Transit publication
+        scope = in_scope if command == "yes" else not_inscope
+        scope.entries.append(publication)
+        # Write bibtex files
+        bh.write_bibtex(bh.IN_REVIEW_FP, in_review)
+        bh.write_bibtex(bh.IN_SCOPE_FP, in_scope)
+        bh.write_bibtex(bh.NOT_IN_SCOPE_FP, not_in_scope)
+        # Commit, open pull-request and auto-merge
+        decision = "in-scope" if command == "yes" else "not-in-scope"
+        title = f"Move {publication['ID']} to {decision} #{transition.issue.number}"
+        repo.index.add([bh.IN_REVIEW_FP, bh.IN_SCOPE_FP, bh.NOT_IN_SCOPE_FP])
+        repo.index.commit(title)
+        repo.remote("origin").push()
+        pr = gh_repo.create_pull(base="master", head="develop", title=title)
+        pr.merge(merge_method="rebase")
+        # Cleanup local repository
+        repo.heads.master.checkout()
+        repo.remote("origin").pull()
+        repo.heads.develop.checkout()
+        repo.git.rebase("origin/master")
+        repo.remote("origin").push(force=True)
+        repo.heads.master.checkout()
+        # Update issue
+        labels = [decision]
+        if command == "yes":
+            labels.append("check-references")
+        issue.edit(labels=labels)
+        if command == "no":
+            issue.edit(state="closed")
+
+
+if __name__ == "__main__":
+    sys.exit(main())
