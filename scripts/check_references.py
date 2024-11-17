af8fcb71fd9ccc05278f9c28d6a03b94843469f7
Alexander Wagner <alexander.wagner@aisec.fraunhofer.de>
2024-11-17 19:19:51 +0100

    scripts/check_references: Create comment of unknown refs at end

--- scripts/check_references.py
+++ scripts/check_references.py
@@ -37,16 +37,15 @@ def main():
             [r for r in references if "DOI" not in r], indent=4
         )
         text = f"I didnot find DOIs for the following references:\n```\n{no_doi_references}\n```"
-        issue.create_comment(text)
 
         references = [r for r in references if "DOI" in r]
         for reference in references:
             result = review_append_with_doi.main([reference["DOI"]])
             if result == os.EX_OK:
                 continue
-            text = f"I failed to append the following reference to the review pipeline:\n```\n{json.dumps(reference, indent=4)}\n```"
-            issue.create_comment(text)
+            text = f"{text}\n\nI failed to append the following reference to the review pipeline:\n```\n{json.dumps(reference, indent=4)}\n```"
 
+        issue.create_comment(text)
         labels = issue.get_labels()
         labels = [l.name for l in labels if l.name != "check-references"]
         issue.edit(labels=labels)
