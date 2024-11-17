27d5b3a84e8cb6a0e9e2ec6cf8fc484f36dc5605
Alexander Wagner <alexander.wagner@aisec.fraunhofer.de>
2024-11-17 17:03:00 +0100

    fixup

--- scripts/check_references.py
+++ scripts/check_references.py
@@ -36,7 +36,7 @@ def main():
         no_doi_references = json.dumps(
             [r for r in references if "DOI" not in r], indent=4
         )
-        text = f"I didnot find DOIs for the following references:\n```\n{no_doi_references}```"
+        text = f"I didnot find DOIs for the following references:\n```\n{no_doi_references}\n```"
         issue.create_comment(text)
 
         references = [r for r in references if "DOI" in r]
@@ -44,7 +44,7 @@ def main():
             result = review_append_with_doi.main([reference["DOI"]])
             if result == os.EX_OK:
                 continue
-            text = f"I failed to append the following reference to the review pipeline:\n```{json.dumps(reference, indent=4)}\n```"
+            text = f"I failed to append the following reference to the review pipeline:\n```\n{json.dumps(reference, indent=4)}\n```"
             issue.create_comment(text)
 
         labels = issue.get_labels()
